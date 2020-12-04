07be6eb93dcd31441726925af3fd340df939b890
Noah Nsimbe <nsimbenoah@gmail.com>
2020-12-04 15:29:32 +0300

    set production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiRoot: 'http://127.0.0.1:8000/',
+  apiRoot: 'https://ancient-waters-88205.herokuapp.com/',
   combination: 'get_combination/',
   course: 'get_course/',
   careers: 'careers/',
